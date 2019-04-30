ab03a918f9a71b2c6dc57d9a126dffd18b3e2e98
Ali Dar <ali.dar@lovemobile.com.au>
2019-05-01 00:23:53 +1000

    changes to config files

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t alidar/multi-client:latest -t alidar/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t alidar/multi-server:latest -t alidar/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t alidar/multi-worker:latest -t alidar/multi-worker:$SHA -f ./worder/Dockerfile ./worker
+
+docker push alidar/multi-client:latest
+docker push alidar/multi-server:latest
+docker push alidar/multi-worker:latest
+
+docker push alidar/multi-client:$SHA
+docker push alidar/multi-server:$SHA
+docker push alidar/multi-worker:$SHA
+
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=alidar/multi-server:$SHA
+kubectl set image deployments/client-deployment client=alidar/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=alidar/multi-worker:$SHA
